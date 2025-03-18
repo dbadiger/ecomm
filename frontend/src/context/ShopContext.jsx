11787b9bb689e88aecbbfac211c56b027e8eeed7
Darshan Badiger <140099027+dbadiger@users.noreply.github.com>
2025-03-18 16:24:20 +0000

    product page completed

---
+++ frontend/src/context/ShopContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react";
+import { products } from "../assets/assets";
+
+export const ShopContext = createContext()
+
+const ShopContextProvider = (props)=>{
+
+    const currency='₹';
+    const delivery_fees=20;
+    const [search, setSearch]=useState('');
+    const [showSearch, setShowSearch]=useState(false)
+    const value={
+        products, currency, delivery_fees,search, setSearch,showSearch, setShowSearch
+    }
+
+    return(
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider;
